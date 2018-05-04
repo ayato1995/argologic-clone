4d1876f7a57bbdcb12df9ae92ee7574513409ce1
ayato1995 <ayato.k070206@outlook.jp>
2018-05-04 17:41:03 +0900

    playerクラスを実装 & 画面の差し替え機能を実装

--- js/main.js
+++ js/main.js
@@ -1,62 +1,44 @@
 enchant();
 
-function toMove(core, player, map) {
-  if (core.input.left) {
-    player.x -= 4;
-    if (map.hitTest(player.x + 8, player.y)) player.x += 4;
-    player.frame = 9;
-  }
-  if (core.input.right) {
-    player.x += 4;
-    if (map.hitTest(player.x + 20, player.y)) player.x -= 4;
-    player.frame = 18;
-  }
-  if (core.input.up) {
-    player.y -= 4;
-    if (map.hitTest(player.x + 8, player.y)) player.y += 4;
-    player.frame = 27;
-  }
-  if (core.input.down) {
-    player.y += 4;
-    if (map.hitTest(player.x + 8, player.y + 8)) player.y -= 4;
-    player.frame = 0;
-  }
-}
-
-function toButtonMove(e, player) {
-  player.x = e.x - player.width / 2;
-  player.y = e.y - player.height / 2;
-}
-
 window.onload = function() {
   core = new Core(320, 320);
   core.fps = 60;
   core.preload("../img/chara5.png", "../img/map0.gif");
 
   core.onload = function() {
-    var player = new Sprite(32, 32);
-    player.image = core.assets["../img/chara5.png"];
-    player.x = 56;
-    player.y = 132;
-
+    /* map initialize */
     var backgroundMap = new Map(16, 16);
     backgroundMap.image = core.assets["../img/map0.gif"];
     backgroundMap.loadData(stage_map1_0, stage_map1_1);
     backgroundMap.collisionData = stage_map1_col;
-    console.log(backgroundMap.image);
 
+    /* player initialize */
+    var player = new Player(56, 132, backgroundMap);
     core.rootScene.addChild(backgroundMap);
     core.rootScene.addChild(player);
 
     player.addEventListener("enterframe", function(e) {
       toMove(core, player, backgroundMap);
-      // console.log(player.x + "    " + player.y);
     });
 
     player.addEventListener("touchmove", function(e) {
       toButtonMove(e, player);
     });
 
+    core.rootScene.addEventListener("enterframe", function(e) {
+      if (player.x > 80) {
+        core.replaceScene(core.field())
+        console.log(core);
+      }
+    });
+  }
+
+  core.field = function() {
+    var scene = new Scene();
+    scene.backgroundColor = "red";
+    console.log(scene.backgroundColor);
+
+    return scene;
   }
 
   core.start();
---
+++ js/player.js
@@ -0,0 +1,38 @@
+﻿
+function toMove(core, player, map) {
+  if (core.input.left) {
+    player.x -= 4;
+    if (map.hitTest(player.x + 8, player.y)) player.x += 4;
+    player.frame = 9;
+  }
+  if (core.input.right) {
+    player.x += 4;
+    if (map.hitTest(player.x + 20, player.y)) player.x -= 4;
+    player.frame = 18;
+  }
+  if (core.input.up) {
+    player.y -= 4;
+    if (map.hitTest(player.x + 8, player.y)) player.y += 4;
+    player.frame = 27;
+  }
+  if (core.input.down) {
+    player.y += 4;
+    if (map.hitTest(player.x + 8, player.y + 8)) player.y -= 4;
+    player.frame = 0;
+  }
+}
+
+function toButtonMove(e, player) {
+  player.x = e.x - player.width / 2;
+  player.y = e.y - player.height / 2;
+}
+
+var Player = enchant.Class.create(enchant.Sprite, {
+  initialize: function(x, y, map) {
+    enchant.Sprite.call(this, 32, 32);
+    this.image = core.assets["../img/chara5.png"];
+    this.frame = 27;
+    this.x = x;
+    this.y = y;  
+  }
+})
