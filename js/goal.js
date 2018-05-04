ed406caa9c27291e20e2cd67dd8339cf396855e5
ayato1995 <ayato.k070206@outlook.jp>
2018-05-04 18:38:26 +0900

    ゴールの当たり判定とゴールした後の画面遷移を実装

---
+++ js/goal.js
@@ -0,0 +1,9 @@
+﻿
+var Goal = enchant.Class.create(enchant.Sprite, {
+  initialize: function(x, y, map) {
+    enchant.Sprite.call(this, 16, 16);
+    this.image = core.assets["../img/goal.png"];
+    this.x = x;
+    this.y = y;  
+  }
+})
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ enchant();
 window.onload = function() {
   core = new Core(320, 320);
   core.fps = 60;
-  core.preload("../img/chara5.png", "../img/map0.gif");
+  core.preload("../img/chara5.png", "../img/map0.gif", "../img/goal.png");
 
   core.onload = function() {
     /* map initialize */
@@ -12,13 +12,19 @@ window.onload = function() {
     backgroundMap.loadData(stage_map1_0, stage_map1_1);
     backgroundMap.collisionData = stage_map1_col;
 
+    /* goal initialize */
+    var goal = new Goal(240, 144, backgroundMap);
+
     /* player initialize */
     var player = new Player(56, 132, backgroundMap);
+
     core.rootScene.addChild(backgroundMap);
+    core.rootScene.addChild(goal);
     core.rootScene.addChild(player);
 
     player.addEventListener("enterframe", function(e) {
       toMove(core, player, backgroundMap);
+      // console.log(player.x + "   " + player.y);
     });
 
     player.addEventListener("touchmove", function(e) {
@@ -26,16 +32,17 @@ window.onload = function() {
     });
 
     core.rootScene.addEventListener("enterframe", function(e) {
-      if (player.x > 80) {
+      if (player.intersect(goal)) {
+        console.log("");
         core.replaceScene(core.field())
-        console.log(core);
       }
     });
+
   }
 
   core.field = function() {
     var scene = new Scene();
-    scene.backgroundColor = "red";
+    scene.backgroundColor = "yellow";
     console.log(scene.backgroundColor);
 
     return scene;
