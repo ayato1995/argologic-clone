87e8aca6281862837f054782d91823bd0cf51e79
ayato1995 <ayato.k070206@outlook.jp>
2018-08-04 21:27:37 +0900

    loop_startをドラッグ&ドロップに対応

--- js/loop_start.js
+++ js/loop_start.js
@@ -22,33 +22,62 @@ var Loop_start = enchant.Class.create(Block, {
 		return label;
 	},
 
-	register_remove_eventListener: function(array, frame, stage, player) {
+	register_select_eventListener: function(array, frame, stage, player) {
 		this.addEventListener("touchstart", function() {
 			if (stage.play_flag) return;
-			if (stage.selectFlag) {
-				if (this.select) {
-					var i = this.searchBlock(player.copy_list);
-					player.copy_list.splice(i, player.copy_list.length - i);
-					i = this.searchBlock(array);
-					for (var j = i; j < array.length && j < i + 3; j++) {
-						array[j].select = false;
-						array[j].backgroundColor = array[j].default_color;
-					}
-				} else {
-          			this.reset_block_color(stage.frames);
-					player.copy_list.length = 0;
-					var i = this.searchBlock(array);
-					for (var j = i; j < array.length && j < i + 3; j++) {
-						player.copy_list.push(array[j]);
-						array[j].select = true;
-						array[j].backgroundColor = "yellow";
-					}
+			if (!stage.selectFlag) return;
+			if (this.select) {
+				var i = this.searchBlock(player.copy_list);
+				player.copy_list.splice(i, player.copy_list.length - i);
+				i = this.searchBlock(array);
+				for (var j = i; j < array.length && j < i + 3; j++) {
+					array[j].select = false;
+					array[j].backgroundColor = array[j].default_color;
 				}
+			} else {
+      			this.reset_block_color(stage.frames);
+				player.copy_list.length = 0;
+				var i = this.searchBlock(array);
+				for (var j = i; j < array.length && j < i + 3; j++) {
+					player.copy_list.push(array[j]);
+					array[j].select = true;
+					array[j].backgroundColor = "yellow";
+				}
+			}
+		});
+	},
+	register_all_remove_eventListener: function(array, frame, stage, player) {
+		for (var i = 0; i < stage.frames.length; i++) {
+			if (frame == stage.frames[i]) continue;
+			this.register_remove_eventListener(stage.frames[i].blocks, stage.frames[i], stage, player);
+		}
+		this.addEventListener("touchend", function(e) {
+			if (e.x > frame.x && e.x < frame.x + frame.width
+				&& e.y > frame.y && e.y < frame.y + frame.height) {
+				this.x = this.default_x;
+				this.y = this.default_y;
+				this.loop_label.x = this.x + this.width - this.loop_label.width;
+				this.loop_label.y = this.y + this.height - 6;
 			} else {
 				stage.log += "delete " + this.type + " " + frame.name + "\n";
 				stage.removeChild(this.loop_label);
 				stage.removeChild(this);
 				this.block_remove(array);
+			}
+		});
+	},
+
+	register_remove_eventListener: function(array, frame, stage, player) {
+		this.addEventListener("touchend", function(e) {
+			if (stage.play_flag) return;
+			if (stage.selectFlag) return;
+			if (e.x > frame.x && e.x < frame.x + frame.width &&
+				e.y > frame.y && e.y < frame.y + frame.height) {
+				if (frame.nest.length == 0 || frame.nest[frame.nest.length - 1] == 0) {
+					stage.log += "insert " + this.type + " " + frame.name + "\n";
+					frame.loop_stack.push(this.type);
+					this.set_block(array, frame, stage, player);
+				}
 			}
 		});
 	},
@@ -98,7 +127,7 @@ var Loop_start = enchant.Class.create(Block, {
 	set_block: function(array, frame, stage, player) {
 		var block = new Loop_start(frame.x + 4, array.length * 20 + frame.y + 4);
 		block.set_loop_cnt(this, block);
-		block.register_remove_eventListener(array, frame, stage, player);
+		block.register_new_block_eventListener(array, frame, stage, player);
 		stage.addChild(block);
 		stage.addChild(block.loop_label);
 		array.push(block);
